2527e8b1660415720ce28b3f00ea5fdc21d92c5c
Jarek Miszczak <miszczak@iitis.pl>
2017-08-17 14:43:15 +0200

    Minor chanes in documentation.

--- src/utils.jl
+++ src/utils.jl
@@ -12,8 +12,8 @@ SparseDenseVector = Union{SparseVector{T} where T<:Number, Vector{S} where S<:Nu
 """
     type Vertex
 
-Type consisting of list of `Int` which, describing the labels of vectors from
-the cannonical basis which correspond to the `Vertex`. See [1] for the more
+Type consisting of list of `Int`, describing the labels of vectors from the
+cannonical basis which correspond to the `Vertex`. See [1] for the more
 information and usage exmaples.
 
 [1] K. Domino, A. Glos, M. Ostaszewski, Superdiffusive quantum stochastic walk
@@ -50,7 +50,7 @@ checkVertexSet(vset::Vector{Vertex}) = checkVertexSet([vertex.linspace for verte
 """
     type VertexSet
 
-Type consisting of a list of `Vertex` objects. It describes the partition of
+Type consisting of a list of `Vertex` objects. It describes the partition of the
 linear subspace.
 
 [1] K. Domino, A. Glos, M. Ostaszewski, Superdiffusive quantum stochastic walk
@@ -64,7 +64,6 @@ type VertexSet
   VertexSet(vset::Vector{Vector{Int}}) = checkVertexSet(vset) ? new([Vertex(v) for v=vset]) : throw(ArgumentError("Vertices should correspond to orthogonal linear spaces"))
 end
 
-
 (v::VertexSet)() = v.vertices
 
 ==(v::VertexSet,w::VertexSet) = v.vertices == w.vertices
@@ -72,11 +71,11 @@ end
 getindex(vset::VertexSet, i::Int) = vset()[i]
 getindex(vset::VertexSet, veci::Vector{Int}) = vset()[veci]
 length(vset::VertexSet) = length(vset())
-"""
 
+"""
     partitionsize(vertexset)
 
-Return the dimenions of linearspace corresponding to given `vertexset'.
+Return the dimenion of the linearspace corresponding to given `vertexset'.
 
 # Examples
 
