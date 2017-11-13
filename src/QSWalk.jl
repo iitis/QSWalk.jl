9bb33a128502bb7832d726cb45da454a5ad27907
Jarek Miszczak <miszczak@iitis.pl>
2017-11-13 12:56:53 +0100

    Reorganization of functions for demoralization

--- src/QSWalk.jl
+++ src/QSWalk.jl
@@ -6,28 +6,27 @@ include("dirac.jl")
 include("operator.jl")
 include("demoralization.jl")
 include("evolution.jl")
-include("compactuserfunctions.jl")
 
 export
-  SparseDenseMatrix, 
-  SparseDenseVector, 
-  Vertex, 
-  VertexSet, 
-  ket, 
-  bra, 
-  ketbra, 
-  proj, 
-  res, 
-  unres, 
-  classical_lindbladian, 
-  evolve_generator, 
-  local_hamiltonian, 
-  nonmoralizing_lindbladian, 
-  make_vertex_set, 
-  init_nonmoralized, 
-  measurement_nonmoralized, 
-  evolve_operator, 
-  evolve, 
+  SparseDenseMatrix,
+  SparseDenseVector,
+  Vertex,
+  VertexSet,
+  ket,
+  bra,
+  ketbra,
+  proj,
+  res,
+  unres,
+  classical_lindbladian,
+  evolve_generator,
+  local_hamiltonian,
+  nonmoralizing_lindbladian,
+  make_vertex_set,
+  init_nonmoralized,
+  measurement_nonmoralized,
+  evolve_operator,
+  evolve,
   global_hamiltonian
 
 end
