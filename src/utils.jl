14b8d4d5bbcf61819c3e8ebb19ac75ea195106d2
Jarek Miszczak <miszczak@iitis.pl>
2017-11-13 12:56:24 +0100

    Reorganization of functions for demoralization

--- src/utils.jl
+++ src/utils.jl
@@ -106,6 +106,163 @@ function vertexsetsize(vset::VertexSet)
   sum(length.(vlist(vset)))
 end
 
+
+"""
+
+    incidence_list(A[; epsilon])
+
+Return list of indices of non-zero elements of the matrix `A`. The `i`-th element of the
+result list is the vector of indices `j` for which `abs(A[j, i]) >=  epsilon`.
+
+# Examples
+
+```jldoctest
+julia> A = [1 2 3; 0 3. 4.; 0 0 5.]
+3×3 Array{Float64, 2}:
+ 1.0  2.0  3.0
+ 0.0  3.0  4.0
+ 0.0  0.0  5.0
+
+julia> QSWalk.incidence_list(A)
+3-element Array{Array{Int64, 1}, 1}:
+ [1]
+ [1, 2]
+ [1, 2, 3]
+
+julia> QSWalk.incidence_list(A, epsilon = 2.5)
+3-element Array{Array{Int64, 1}, 1}:
+  Int64[]
+  [2]
+  [1, 2, 3]
+
+```
+"""
+function incidence_list(A::SparseMatrixCSC{T} where T<:Number;
+                        epsilon::Real = eps())
+  @argumentcheck epsilon >=  0 "epsilon needs to be nonnegative"
+  @argumentcheck size(A, 1) ==  size(A, 2) "A matrix must be square"
+  [filter(x -> abs(A[x, i])>= epsilon, A[:, i].nzind) for i = 1:size(A, 1)]
+end
+
+function incidence_list(A::Matrix{T} where T<:Number;
+                        epsilon::Real = eps())
+  @argumentcheck epsilon >=  0 "epsilon needs to be nonnegative"
+  @argumentcheck size(A, 1) ==  size(A, 2) "A matrix must be square"
+  [find(x -> abs(x)>= epsilon, A[:, i]) for i = 1:size(A, 1)]
+end
+
+"""
+
+    reversed_incidence_list(A[; epsilon])
+
+For given matrix `A` the function returns list of indices. The `i`-th element of
+result list is the vector of indices for which `abs(A[i, j]) >=  epsilon`.
+
+# Examples
+
+```jldoctest
+julia> A = [1 2 3; 0 3. 4.; 0 0 5.]
+3×3 Array{Float64, 2}:
+ 1.0  2.0  3.0
+ 0.0  3.0  4.0
+ 0.0  0.0  5.0
+
+julia> QSWalk.reversed_incidence_list(A)
+3-element Array{Array{Int64, 1}, 1}:
+ [1, 2, 3]
+ [2, 3]
+ [3]
+
+julia> QSWalk.reversed_incidence_list(A, epsilon = 2.5)
+3-element Array{Array{Int64, 1}, 1}:
+ [3]
+ [2, 3]
+ [3]
+```
+"""
+function reversed_incidence_list(A::SparseDenseMatrix; epsilon::Real = eps())
+  incidence_list(transpose(A), epsilon = epsilon)
+end
+
+"""
+
+    revinc_to_vertexset(revincidence_list)
+
+Return `vertexset` of type `VertexSet` corresponding to given list of indices.
+The function map to consecutive element list orthogonal subspaces. The
+dimensions of the subspaces equal to size of each element of revincidence_list.
+The exception is empty list, for which onedimensional subspace is attached.
+
+# Examples
+
+```jldoctest
+julia> vset = [Int64[], [2], [1, 2, 3]]
+3-element Array{Array{Int64, 1}, 1}:
+ Int64[]
+ [2]
+ [1, 2, 3]
+
+julia> QSWalk.revinc_to_vertexset(vset)()
+3-element Array{QSWalk.Vertex, 1}:
+ QSWalk.Vertex([1])
+ QSWalk.Vertex([2])
+ QSWalk.Vertex([3, 4, 5])
+
+```
+"""
+function revinc_to_vertexset(revincidence_list::Vector{Vector{Int}})
+  vertexset = Vector{Int}[]
+  start = 1
+  for i = revincidence_list
+    if length(i)!= 0
+      push!(vertexset, collect(start:(start+length(i)-1)))
+      start+= length(i)
+    else
+      push!(vertexset, [start])
+      start +=  1
+    end
+  end
+  VertexSet(vertexset)
+end
+
+"""
+
+    make_vertex_set(A[, epsilon])
+
+Creates object of type `VertexSet` which represents how vertices are located in
+matrix. Should be used only in the nondefault use of `global_operator` function.
+It is always equal to the second element if output of `global_operator` function.
+
+# Examples
+
+```jldoctest
+julia> A = [1 2 3; 0 3. 4.; 0 0 5.]
+3×3 Array{Float64, 2}:
+ 1.0  2.0  3.0
+ 0.0  3.0  4.0
+ 0.0  0.0  5.0
+
+julia> make_vertex_set(A)()
+3-element Array{QSWalk.Vertex, 1}:
+ QSWalk.Vertex([1, 2, 3])
+ QSWalk.Vertex([4, 5])
+ QSWalk.Vertex([6])
+
+julia> make_vertex_set(A, epsilon = 2.5)()
+3-element Array{QSWalk.Vertex, 1}:
+ QSWalk.Vertex([1])
+ QSWalk.Vertex([2, 3])
+ QSWalk.Vertex([4])
+
+```
+"""
+function make_vertex_set(A::SparseDenseMatrix; epsilon::Real = eps())
+  @argumentcheck epsilon >=  0 "epsilon needs to be nonnegative"
+  @argumentcheck size(A, 1) ==  size(A, 2) "A matrix must be square"
+  revinc_to_vertexset(reversed_incidence_list(A, epsilon = epsilon))
+end
+
+
 macro argumentcheck(ex, msgs...)
     msg = isempty(msgs) ? ex : msgs[1]
     if isa(msg, AbstractString)
