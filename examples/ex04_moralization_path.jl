66b14a63f16d07d864fd3e8da9ac3d804431ac87
Adam Glos <aglos@iitis.pl>
2017-11-14 12:24:35 +0100

    Nonmoralized example on path graph improved by nice plot generators

--- examples/ex04_moralization_path.jl
+++ examples/ex04_moralization_path.jl
@@ -3,11 +3,12 @@
 # ------------------------------------------------------------------------------
 
 using QSWalk
+using PyPlot # for plot
 using LightGraphs # for PathGraph
 
 ## nonsymmetric case
 
-dim = 100 #odd for unique middle point
+dim = 101 #odd for unique middle point
 w = 0.5
 time = 40.
 adjacency = adjacency_matrix(PathGraph(dim))
@@ -19,20 +20,31 @@ hlocal = local_hamiltonian(vset)
 opnonsymmetric = evolve_generator(hglobal, [lind], hlocal, w)
 rhoinit = init_nonmoralized(vset[[midpoint]], vset)
 
-rho = evolve(opnonsymmetric, rhoinit, time)
+rho_nonsymmetric = evolve(opnonsymmetric, rhoinit, time)
 positions = (collect(1:dim)-midpoint)
-println(sum(positions .* measurement_nonmoralized(rho, vset)))
+measurement_nonsymmetric = measurement_nonmoralized(rho_nonsymmetric, vset)
+println(sum(positions .* measurement_nonsymmetric))
+
+figure(figsize=[2.5, 1.5])
+plot(positions, measurement_nonsymmetric, "k")
+plot(positions, reverse(measurement_nonsymmetric), "b--")
+xlabel("position")
+ylabel("probability")
+tick_params(labelsize=9)
+axis([positions[1], positions[end], 0, maximum(measurement_nonsymmetric)])
+vlines(0,0.,maximum(measurement_nonsymmetric),linestyles="--")
+savefig("nonsymmetric.pdf", bbox_inches="tight")
 
 ## symmetric case
 
-dim = 100 #odd for unique middle point
+dim = 101 #odd for unique middle point
 w = 0.5
 time = 40.
 adjacency = adjacency_matrix(PathGraph(dim))
 midpoint = ceil(Int, dim/2)
 
 linddescription1 = Dict(1 => ones(1,1), 2 => [1 1; 1 -1])
-linddescription2 = Dict(1 => ones(1,1), 2 => [1 -1; 1 1])
+linddescription2 = Dict(1 => ones(1,1), 2 => [1 1; -1 1])
 lind1, vset = nonmoralizing_lindbladian(adjacency, linddescription1)
 lind2, vset = nonmoralizing_lindbladian(adjacency, linddescription2)
 hglobal = global_hamiltonian(adjacency)
@@ -40,6 +52,17 @@ hlocal = local_hamiltonian(vset)
 opsymmetric = evolve_generator(hglobal, [lind1, lind2], hlocal, w)
 rhoinit = init_nonmoralized(vset[[midpoint]], vset)
 
-rho = evolve(opsymmetric, rhoinit, time)
+rho_symmetric = evolve(opsymmetric, rhoinit, time)
 positions = (collect(1:dim)-midpoint)
-println(sum(positions .* measurement_nonmoralized(rho, vset)))
+measurement_symmetric = measurement_nonmoralized(rho_symmetric, vset)
+println(sum(positions .* measurement_symmetric))
+
+figure(figsize=[2.5, 1.5])
+plot(positions, measurement_symmetric, "k")
+plot(positions, reverse(measurement_symmetric), "--b")
+xlabel("position")
+ylabel("probability")
+tick_params(labelsize=9)
+axis([positions[1], positions[end], 0, maximum(measurement_symmetric)])
+vlines(0,0.,maximum(measurement_symmetric),linestyles="--")
+savefig("symmetric.pdf", bbox_inches="tight")
