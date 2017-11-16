640c75b27ff6070e1efbf769e84ee6fc9dec5173
Jarek Miszczak <miszczak@iitis.pl>
2017-11-16 13:08:23 +0100

    Consistency check for ex02

--- examples/ex02_convergence.jl
+++ examples/ex02_convergence.jl
@@ -1,30 +1,34 @@
 # ------------------------------------------------------------------------------
-# Case 4: convergence for local interaction
+# Example 2: convergence for the local interaction regime
 # ------------------------------------------------------------------------------
 
 using QSWalk
-using LightGraphs # for erdos_renyi
+using LightGraphs # for graph functions
 
 ## operator preparations
+# number of nodes
 dim = 10
-digraph = erdos_renyi(dim, 0.5, is_directed=true)
+
+# smaller vale of prob can be used to geneate graphs whcih are not strongly connected
+prob = 0.5
+digraph = erdos_renyi(dim, prob, is_directed=true)
 graph = Graph(digraph)
-adjacencydigraph = full(adjacency_matrix(digraph))
-adjacencygraph = full(adjacency_matrix(graph))
+adj_digraph = full(adjacency_matrix(digraph))
+adj_graph = full(adjacency_matrix(graph))
 time = 100.
 
-lind = classical_lindbladian(adjacencydigraph)
-globaloperator = evolve_generator(adjacencygraph, lind)
+lind = classical_lindbladian(adj_digraph)
+evo_gen = evolve_generator(adj_graph, lind)
 
-## dimensionality of null-space
-println(count(x->abs(x)<1e-5, eigvals(globaloperator)))
+## dimensionality of the null space
+println(count(x->abs(x)<1e-5, eigvals(evo_gen)))
 
-## different initial states examples
+## different initial states
 rhoinit1 = proj(1, dim)
 rhoinit2 = proj(3, dim)
 rhoinit3 = eye(dim)/dim
 
-U = evolve_operator(globaloperator, time)
+U = evolve_operator(evo_gen, time)
 rho1 = evolve(U, rhoinit1)
 rho2 = evolve(U, rhoinit2)
 rho3 = evolve(U, rhoinit3)
